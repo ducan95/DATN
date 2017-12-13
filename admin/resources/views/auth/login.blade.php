78e0dbd664019e82d3a0b5dbd54b14b5c0820bc1
Thuan Le <thuan09694@hotmail.com>
2017-12-13 16:25:16 +0900

    modified ui login page

--- admin/resources/views/auth/login.blade.php
+++ admin/resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Sougou Zyanaru</title>
+  <title>総合ジャーナル</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -29,12 +29,9 @@
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
-  <div class="login-logo">
-    <a href="index2.html"><b>Đăng nhập</b></a>
-  </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <p class="login-box-msg">Sign in to start your session</p>
+    <h2 class="login-box-msg">総合ジャーナル</h2>
      
      @if (count($errors) >0)
         <div class="box-body">
@@ -61,7 +58,7 @@
              <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
          </div>
          <div class="form-group has-feedback">
-             <input type="password" class="form-control" placeholder="Password" name="txtPassword">
+             <input type="password" class="form-control" placeholder="パスワード" name="txtPassword">
              <span class="glyphicon glyphicon-lock form-control-feedback"></span>
          </div>
          <div class="row">
@@ -73,23 +70,12 @@
                  </div>
              </div>
              <div class="col-xs-4">
-                 <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                 <button type="submit" class="btn btn-primary btn-block btn-flat">ログイン</button>
              </div>
          </div>
      </form>
-
-
-    <div class="social-auth-links text-center">
-      <p>- OR -</p>
-     {{--   <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
-        Facebook</a>  --}}
-      <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-        Google+</a>
-    </div>
-    <!-- /.social-auth-links -->
-
-    <a href="#">I forgot my password</a><br>
-    <a href="register.html" class="text-center">Register a new membership</a>
+    <!-- <a href="#">I forgot my password</a><br> -->
+    <!-- <a href="register.html" class="text-center">Register a new membership</a> -->
 
   </div>
   <!-- /.login-box-body -->
