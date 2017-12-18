bd36d76cb5c80ecb0756778c318b45ba3863f1af
Thuan Le <thuan09694@hotmail.com>
2017-12-18 19:11:48 +0900

    ベトナム語 > 日本語,

--- admin/resources/views/templates/master.blade.php
+++ admin/resources/views/templates/master.blade.php
@@ -31,7 +31,7 @@
   <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body class="hold-transition skin-blue sidebar-mini" ng-cloak>
+<body class="hold-transition skin-purple-light sidebar-mini" ng-cloak>
 <div></div>
 <div id="spinner_sougouzyanaru">
     <div id="spinner_sougouzyanaru-opacity"></div>
