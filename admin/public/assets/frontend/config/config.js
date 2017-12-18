743a0fd49f34a6fe4a81b34f3549a9dd33c5341a
Thuan Le <thuan09694@hotmail.com>
2017-12-18 14:41:49 +0900

    spinner

--- admin/public/assets/frontend/config/config.js
+++ admin/public/assets/frontend/config/config.js
@@ -2,3 +2,42 @@
  * Created by rikkei on 15/12/2017.
  */
 var SOUGOU_ZYANARU_MODULE = angular.module("sougou_zyanaru", ['ngResource']);
+
+
+SOUGOU_ZYANARU_MODULE.factory('HttpInterceptor', function ($rootScope, $q) {
+  var requestCount = 0;
+  return {
+    request: function (config) {
+      var opts = config._opts;
+      requestCount++;
+      if (!opts || opts.loading !== false) {
+        $("#spinner_sougouzyanaru").addClass('show-spinner');
+      }
+      return config || $q.when(config);
+    },
+    response: function (response) {
+      requestCount--;
+      if (requestCount == 0) {
+        setTimeout(function () {
+          $("#spinner_sougouzyanaru").removeClass('show-spinner');
+        },200);
+      }
+
+      return response || $q.when(response);
+    },
+    responseError: function (response) {
+      requestCount--;
+      if (requestCount == 0) {
+        setTimeout(function () {
+          $("#spinner_sougouzyanaru").removeClass('show-spinner');
+        },200);
+      }
+      return $q.reject(response);
+    }
+  };
+});
+
+
+SOUGOU_ZYANARU_MODULE.config(['$httpProvider', function ($httpProvider) {
+  $httpProvider.interceptors.push('HttpInterceptor');
+}]);
