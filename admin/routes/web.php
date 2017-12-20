2e9c177c7e2639c6dacd74be568b416805b9bf67
Thuan Le <thuan09694@hotmail.com>
2017-12-20 13:21:48 +0900

    fix route

--- admin/routes/web.php
+++ admin/routes/web.php
@@ -49,7 +49,7 @@ Route::group([
 ], function() {
   /** home **/
   Route::get('/',[
-    'uses' => 'HomeController@index',
+    'uses' => 'AdminController@index',
     'as' => 'getIndex'
   ]);
   /** router web user **/
