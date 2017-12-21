ff8a9527d91f627c3e1253d463173f119374c0c4
Duc An <ducan11395@gmail.com>
2017-12-21 08:21:22 +0700

    commit category

--- admin/resources/views/templates/admin/category/addcategorychildern.blade.php
+++ admin/resources/views/templates/admin/category/addcategorychildern.blade.php
@@ -1,80 +1,58 @@
 @extends('templates.master')
 @section('content')
 
-<div>
+<div ng-controller='CategoryChildrenAddCtrl'>
 	<section class="content-header">
 		<h2>List Category</h2>
 	</section>
-
 	<section class="content">
 		<div class="row">
 			<div class="col-md-12">
 				<div class="box">
-						<form class="form-horizontal">
-						 <div class="box-body">
-	                <div class="form-group">
-	                  <label for="" class="col-sm-3 control-label">Category Name</label>
-	                  <div class="col-sm-9">
-	                    <input class="form-control" id="" placeholder="" type="text">
-	                  </div>
-	                </div>
-	                <div class="form-group">
-	                  <label for="" class="col-sm-3 control-label">Name Alphabet For Address</label>
-	                  <div class="col-sm-9">
-	                    <input type="text" class="form-control" id="" placeholder="">
-	                  </div>
-	                </div>
-	                <div class="form-group">
-	                	<label class="col-sm-3 control-label">Display Global Navi</label>
-	                	<div class="col-sm-9">
-	                		<input type="checkbox" checked data-toggle="toggle">
-	                	</div>
-	                </div>
-	                <div class="form-group">
-	                	<label class="col-sm-3 control-label">Display Menu Bar</label>
-	                	<div class="col-sm-9">
-	                		<input type="checkbox" checked data-toggle="toggle">
-	                	</div>
-	                </div>
-	                <div class="form-group">
-	                	<label class="col-sm-3 control-label">Parent Name</label>
-	                	<div class="col-sm-9">
-	                		<select  class="form-control">
-	                			<option>ADM</option>
-	                			<option>Pop</option>
-	                		</select>
-	                	</div>
-	                </div>
-
-	                <div class="row" style="padding-bottom: 20px">
-		              	<div class="col-md-4"></div>
-				            <div class="col-md-4 text-center">
-		                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
-							  Add
-							</button>
-
-							<div id="myModal"  class="modal fade" tabindex="-1" role="dialog">
-							  <div class="modal-dialog" role="document">
-							    <div class="modal-content">
-							      <div class="modal-header">
-							        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-							        <h4 class="modal-title">Register parent category children?</h4>
-							      </div>
-							      <div class="modal-footer">
-							        <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
-							        <button type="submit" class="btn btn-primary">Yes</button>
-							      </div>
-							    </div><!-- /.modal-content -->
-							  </div><!-- /.modal-dialog -->
-							</div><!-- /.modal -->
-		                    <a href="/admincp/category/"><button type="button" class="btn btn-primary" style="margin-left:5px;">Cancel</button></a>
-				              </div>
-		              	<div class="col-md-4"></div>
-	              	</div>
-	              <!-- /.box-footer -->
-	              </div>
-	              <!-- /.box-body -->
-            </form>
+					<form class="form-horizontal" ng-submit='addCategoryChil()' method="post">
+						{{ csrf_field() }}	
+					 <div class="box-body">
+              <div class="form-group">
+                <label for="" class="col-sm-3 control-label">Category Name</label>
+                <div class="col-sm-9">
+                  <input class="form-control" ng-model="categories.name" placeholder="...name" name="name" type="text">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="" class="col-sm-3 control-label">Name Alphabet For Address</label>
+                <div class="col-sm-9">
+                  <input type="text" class="form-control" ng-model='categorychil.slug' name="slug" placeholder="...slug" >
+                </div>
+              </div>
+              <div class="form-group">
+              	<label class="col-sm-3 control-label">Display Global Navi</label>
+              	<div class="col-sm-9">
+              		<input type="checkbox" name="global_status" value="1" ng-model="categorychil.global_status">
+              	</div>
+              </div>
+              <div class="form-group">
+              	<label class="col-sm-3 control-label">Display Menu Bar</label>
+              	<div class="col-sm-9">
+              		<input type="checkbox"  name="menu_status" value="1" ng-model="categorychil.menu_status">
+              	</div>
+              </div>
+              <div class="form-group">
+              	<label class="col-sm-3 control-label">Parent Name</label>
+              	<div class="col-sm-9">
+              		<input type="text" value="" class="form-control">
+              	</div>
+              </div>
+              <div class="row" style="padding-bottom: 20px">
+              	<div class="col-md-4"></div>
+		            <div class="col-md-4">
+                  <button type="submit" class="btn btn-primary" style="margin-left: 95px" data-toggle="modal" data-target="#myModal"> Add</button>
+                </div>  
+								<div class="col-md-4">
+                    <a href="{{route('webCategoryIndex')}}"><button type="button" class="btn btn-primary" style="margin-left: -200px">Cancel</button></a>
+                </div>    
+            	</div>
+            </div>	
+          </form>
 				</div>
 			</div>
 		</div>
@@ -88,6 +66,7 @@
 <script src="{{ asset('assets/frontend/resource/CategoryResource.js') }}"></script>
 
 @endsection 
+				             
 
 	              
 		
@@ -96,3 +75,9 @@
                 
 
 
+
+		              	
+
+
+							
+	             
