f6d55f58030a94d4856e8731b251dd1f2304063e
Thuan Le <thuan09694@hotmail.com>
2017-12-22 18:32:26 +0900

    日本

--- admin/resources/lang/ja/user.php
+++ admin/resources/lang/ja/user.php
@@ -4,11 +4,11 @@
 return [
   'admin_user_list'=> 'ユーザ一覧',
   'admin_user_add_new' => '新規追加',
-  'admin_user_edit' => 'ユーザー情報　　編集',
+  'admin_user_edit' => 'ユーザー情報編集',
   'admin_user_name' => 'ユーザー名',
-  'admin_user_role' => 'ユーザー名',
+  'admin_user_role' => '権限',
   'admin_user_email' => 'メール',
-  'admin_user_password' => 'メール',
+  'admin_user_password' => 'パスワード',
 ]
 
 
