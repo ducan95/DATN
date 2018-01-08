8b6d3393ed32e1168ad45e1815daa23dfa41fbfe
Thuan Le <thuan09694@hotmail.com>
2018-01-08 13:47:08 +0900

    animate

--- admin/resources/views/admin/templates/header.blade.php
+++ admin/resources/views/admin/templates/header.blade.php
@@ -14,15 +14,5 @@
       <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
         <span class="sr-only">Toggle navigation</span>
       </a>
-      <!-- Navbar Right Menu -->
-      <div class="navbar-custom-menu">
-        <ul class="nav navbar-nav">
-          <!-- Control Sidebar Toggle Button -->
-          <li>
-            <a href="{{ route('getLogout') }}"><i class="fa fa-power-off"></i></a>
-            {{--  <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>  --}}
-          </li>
-        </ul>
-      </div>
     </nav>
   </header>
