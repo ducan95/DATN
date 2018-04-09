f6e4cb53a315ed220202cf2c2dd1a65c2eda8bba
ducan95 <ducan11395@gmail.com>
2018-04-09 18:09:36 +0700

    fix file lang

--- admin/public/assets/frontend/extension/lang.js
+++ admin/public/assets/frontend/extension/lang.js
@@ -1,38 +1,38 @@
-SOUGOU_ZYANARU_MODULE.service('trans',function(){
-  this.messageDelete = "該当の画像の削除を行います。よろしいですか?";
-});
+// SOUGOU_ZYANARU_MODULE.service('trans',function(){
+//   this.messageDelete = "該当の画像の削除を行います。よろしいですか?";
+// });
 
-/**
- * [TRANS description]
- * @type {Object}
- */
-var TRANS = {
-    ERROR_CONNECT: "システムエラーが発生します",
-    BUTTON_YES: "はい",
-    BUTTON_NO: "いいえ",
-    BUTTON_CONFIRM: '確認',
+// /**
+//  * [TRANS description]
+//  * @type {Object}
+//  */
+// var TRANS = {
+//     ERROR_CONNECT: "システムエラーが発生します",
+//     BUTTON_YES: "はい",
+//     BUTTON_NO: "いいえ",
+//     BUTTON_CONFIRM: '確認',
 
-    CONFIRM_DELETE: '本当に削除する',
-    CONFIRM_PASSWORD: 'パスワード確認',
-    CONFIRM_CREATE: "登録確認",
-    CONFIRM_UPDATE: "変更確認",
-    BUTTON_DELETE : " 削除",
-    BUTTON_EDIT : "編集",
+//     CONFIRM_DELETE: '本当に削除する',
+//     CONFIRM_PASSWORD: 'パスワード確認',
+//     CONFIRM_CREATE: "登録確認",
+//     CONFIRM_UPDATE: "変更確認",
+//     BUTTON_DELETE : " 削除",
+//     BUTTON_EDIT : "編集",
 
-    SECURITY_NUMBER_ERROR : "セキュリティコードは3桁以上4桁以内、半角数字で入力してください。",
+//     SECURITY_NUMBER_ERROR : "セキュリティコードは3桁以上4桁以内、半角数字で入力してください。",
     
-    TITLE_REQUIRED : 'タイトルの設定を行ってください',
-    TITLE_EXITS : "タイトルは既に利用されています",
-    TITLE_MAXLENGTH : 'タイトルの名称は50文字以下で入力してください',
-    VALID_SORT : "カテゴリ表示順位の設定は不正です",
+//     TITLE_REQUIRED : 'タイトルの設定を行ってください',
+//     TITLE_EXITS : "タイトルは既に利用されています",
+//     TITLE_MAXLENGTH : 'タイトルの名称は50文字以下で入力してください',
+//     VALID_SORT : "カテゴリ表示順位の設定は不正です",
 
-    MSG_DELETE: '該当の画像の削除を行います。よろしいですか?',
-    SUCCESS: '完了!',
-    ERROR: 'エラー',
-    REQUIRED: '空白のところで入力してください。',
-    TYPE_EMAIL: 'メールアドレスの入力を行ってください',
-    DELETE_ADMIN: '削除できません。',
-    NUMBER: '号',
-    OK: 'はい',
-    CANCEL: 'いいえ'
-};
+//     MSG_DELETE: '該当の画像の削除を行います。よろしいですか?',
+//     SUCCESS: '完了!',
+//     ERROR: 'エラー',
+//     REQUIRED: '空白のところで入力してください。',
+//     TYPE_EMAIL: 'メールアドレスの入力を行ってください',
+//     DELETE_ADMIN: '削除できません。',
+//     NUMBER: '号',
+//     OK: 'はい',
+//     CANCEL: 'いいえ'
+// };
