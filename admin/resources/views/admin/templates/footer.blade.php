8968a974a68f277d10d9e5c7ad119a6f8fdeaeaf
ducan95 <ducan11395@gmail.com>
2018-05-15 10:23:53 +0700

    fix footer admin

--- admin/resources/views/admin/templates/footer.blade.php
+++ admin/resources/views/admin/templates/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 0.0.1
     </div>
-    <strong>Copyright &copy; 2017 <a href="javascript:void(0)">Duc An-Amazing Sunday</a>.</strong> All rights
+    <strong>Copyright &copy; 2018 <a href="javascript:void(0)">SUNDAY MAGAZINE</a>.</strong> All rights
     reserved.
   </footer>
 
